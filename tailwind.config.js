5e7e8d437988b1dd47df56751837b5f653b31425
khrisnaa <igdkhrisnaaa@gmail.com>
2025-02-05 15:34:09 +0800

    setup filament

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 import forms from '@tailwindcss/forms';
 import defaultTheme from 'tailwindcss/defaultTheme';
+import preset from './vendor/filament/support/tailwind.config.preset';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [preset],
     darkMode: ['class'],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.tsx',
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
     ],
 
     theme: {
